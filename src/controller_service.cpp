63654e2465e059200cf11579e61295df89a850c3
maciverlab@u.northwestern.edu <maciverlab>
2023-09-08 15:11:51 -0500

    revert to single puff attack

--- src/controller_service.cpp
+++ src/controller_service.cpp
@@ -337,8 +337,8 @@ namespace controller {
                         controller_server->agent.set_right(0);
                         controller_server->agent.capture();
                         controller_server->agent.update();
-                        controller_server->agent.capture();
-                        controller_server->agent.update();
+                        //controller_server->agent.capture();
+                        //controller_server->agent.update();
                         controller_server->agent.end_capture();
                         controller_server->agent.update();
                         controller_server->send_capture(step.frame);
